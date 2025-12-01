86d143d68c74fbb1fe2dccfc651bf7f832f06752
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-12-01 10:43:18 +0100

    feat(2025-01): complete part 2

--- 2025/01/src/main.rs
+++ 2025/01/src/main.rs
@@ -1,37 +1,57 @@
 use std::{io::stdin, str::FromStr};
 
 fn main() {
-    let part1_result = part1(stdin().lines().map(|l| l.expect("Could not read line")));
+    let (part1_result, part2_result) =
+        solve(stdin().lines().map(|l| l.expect("Could not read line")));
     println!("Part 1: {part1_result}");
+    println!("Part 2: {part2_result}");
 }
 
-fn part1(input: impl IntoIterator<Item = String>) -> u32 {
+fn solve(input: impl IntoIterator<Item = String>) -> (u32, u32) {
     let mut position: i32 = 50;
     let mut at_zero = 0;
+    let mut crossed_zero_times = 0;
 
     for line in input {
         let dial_move: DialMove = line.parse().expect("Cannot parse dial move");
-        move_dial(&mut position, &dial_move);
+        let crossed_zero_times_during_move = move_dial(&mut position, &dial_move);
+        crossed_zero_times += crossed_zero_times_during_move;
 
         if position == 0 {
             at_zero += 1;
         }
     }
 
-    at_zero
+    (at_zero, crossed_zero_times)
 }
 
-fn move_dial(position: &mut i32, dial_move: &DialMove) {
+// Returns the number of times the dial crossed the 0 position as part of this move.
+fn move_dial(position: &mut i32, dial_move: &DialMove) -> u32 {
     match dial_move.direction {
         TurnDirection::Left => {
+            let initially_at_zero = *position == 0;
             *position -= dial_move.distance as i32;
             if *position < 0 {
-                *position += (u32::div_ceil(-*position as u32, DIAL_SIZE) * DIAL_SIZE) as i32;
+                let mut crosses_zero_times = u32::div_ceil(-*position as u32, DIAL_SIZE);
+                *position += (crosses_zero_times * DIAL_SIZE) as i32;
+                if initially_at_zero {
+                    crosses_zero_times -= 1
+                }
+                if *position == 0 {
+                    crosses_zero_times += 1
+                }
+                crosses_zero_times
+            } else if *position == 0 {
+                1
+            } else {
+                0
             }
         }
         TurnDirection::Right => {
             *position += dial_move.distance as i32;
-            *position %= DIAL_SIZE as i32;
+            let crosses_zero_times = (*position as u32) / DIAL_SIZE;
+            *position -= (crosses_zero_times * DIAL_SIZE) as i32;
+            crosses_zero_times
         }
     }
 }
@@ -79,11 +99,9 @@ impl FromStr for DialMove {
 
 #[cfg(test)]
 mod tests {
-    use crate::{DialMove, move_dial, part1};
+    use crate::{move_dial, solve};
 
-    #[test]
-    fn test_part1() {
-        let input = "L68
+    const SAMPLE_INPUT: &str = "L68
 L30
 R48
 L5
@@ -95,43 +113,71 @@ R14
 L82
 ";
 
-        let result = part1(input.lines().map(str::to_string));
+    #[test]
+    fn test_part1() {
+        let (result, _) = solve(SAMPLE_INPUT.lines().map(str::to_string));
 
         assert_eq!(result, 3);
     }
 
     #[test]
+    fn test_part2() {
+        let (_, result) = solve(SAMPLE_INPUT.lines().map(str::to_string));
+
+        assert_eq!(result, 6);
+    }
+
+    #[test]
     fn move_dial_left_underflow() {
         let mut position = 0;
-        move_dial(&mut position, &"L1".parse().expect("Cannot parse move"));
+        let crosses_zero_times =
+            move_dial(&mut position, &"L1".parse().expect("Cannot parse move"));
         assert_eq!(position, 99);
+        assert_eq!(crosses_zero_times, 0);
+    }
+
+    #[test]
+    fn move_dial_left_underflow_multiple_start_from_zero() {
+        let mut position = 0;
+        let crosses_zero_times =
+            move_dial(&mut position, &"L101".parse().expect("Cannot parse move"));
+        assert_eq!(position, 99);
+        assert_eq!(crosses_zero_times, 1);
     }
 
     #[test]
     fn move_dial_left_underflow_multiple() {
-        let mut position = 0;
-        move_dial(&mut position, &"L101".parse().expect("Cannot parse move"));
+        let mut position = 1;
+        let crosses_zero_times =
+            move_dial(&mut position, &"L102".parse().expect("Cannot parse move"));
         assert_eq!(position, 99);
+        assert_eq!(crosses_zero_times, 2);
     }
 
     #[test]
     fn move_dial_left_underflow_multiple_to_zero() {
         let mut position = 1;
-        move_dial(&mut position, &"L101".parse().expect("Cannot parse move"));
+        let crosses_zero_times =
+            move_dial(&mut position, &"L101".parse().expect("Cannot parse move"));
         assert_eq!(position, 0);
+        assert_eq!(crosses_zero_times, 2);
     }
 
     #[test]
     fn move_dial_right_underflow() {
         let mut position = 99;
-        move_dial(&mut position, &"R1".parse().expect("Cannot parse move"));
+        let crosses_zero_times =
+            move_dial(&mut position, &"R1".parse().expect("Cannot parse move"));
         assert_eq!(position, 0);
+        assert_eq!(crosses_zero_times, 1);
     }
 
     #[test]
     fn move_dial_right_underflow_multiple() {
         let mut position = 99;
-        move_dial(&mut position, &"R101".parse().expect("Cannot parse move"));
+        let crosses_zero_times =
+            move_dial(&mut position, &"R101".parse().expect("Cannot parse move"));
         assert_eq!(position, 0);
+        assert_eq!(crosses_zero_times, 2);
     }
 }
