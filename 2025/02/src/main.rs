97ed14cd61072360788b5e909fb872b9e1befc50
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-12-02 20:50:32 +0100

    feat(2025-02): complete part 2 (hacky)
    It works, but I'm not proud of it.

--- 2025/02/src/main.rs
+++ 2025/02/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     io::stdin,
     iter::{self},
 };
@@ -7,18 +8,34 @@ fn main() {
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Cannot read input");
 
-    println!("Part 1: {0}", part1(input.trim()));
+    let pairs = parse_input(input.trim());
+
+    println!("Part 1: {0}", part1(&pairs));
+    println!("Part 2: {0}", part2(&pairs));
 }
 
-fn part1(input: &str) -> u64 {
-    let pairs = input.split(',').map(|pair_str| {
-        parse_pair(pair_str)
-            .map_err(|err| format!("Could not parse pair {pair_str}: {err:?}"))
-            .unwrap()
-    });
+fn parse_input(input: &str) -> Vec<(u64, u64)> {
+    input
+        .split(',')
+        .map(|pair_str| {
+            parse_pair(pair_str)
+                .map_err(|err| format!("Could not parse pair {pair_str}: {err:?}"))
+                .unwrap()
+        })
+        .collect::<Vec<_>>()
+}
 
+fn part1(pairs: &Vec<(u64, u64)>) -> u64 {
     pairs
-        .map(|(min, max)| invalid_ids(min, max).into_iter().sum::<u64>())
+        .iter()
+        .map(|(min, max)| invalid_ids_halves(*min, *max).into_iter().sum::<u64>())
+        .sum()
+}
+
+fn part2(pairs: &Vec<(u64, u64)>) -> u64 {
+    pairs
+        .iter()
+        .map(|(min, max)| invalid_ids_any_length(*min, *max).into_iter().sum::<u64>())
         .sum()
 }
 
@@ -36,7 +53,8 @@ fn parse_pair(pair_str: &str) -> Result<(u64, u64), String> {
     Ok((parts[0], parts[1]))
 }
 
-fn invalid_ids(min: u64, max: u64) -> Vec<u64> {
+// Find invalid IDs for part 1
+fn invalid_ids_halves(min: u64, max: u64) -> Vec<u64> {
     let min_str = min.to_string();
 
     // Digits from the higher half of the number.
@@ -82,9 +100,104 @@ fn invalid_ids(min: u64, max: u64) -> Vec<u64> {
     invalid_ids
 }
 
+// NOTE: this function did not work correctly in the actual full input for day 2 for part 2 :/
+fn are_halves_repeated(num: &str) -> bool {
+    if num.len() == 1 {
+        return false;
+    }
+
+    let middle_index = num.len() / 2;
+    let higher_half = if num.len() % 2 == 0 {
+        &num[0..middle_index]
+    } else {
+        // NOTE: for numbers of odd length (e.g. 55555), the middle digit should appear in both
+        // lower and higher halves
+        &num[0..=middle_index]
+    };
+    let lower_half = &num[middle_index..];
+
+    higher_half == lower_half
+}
+
+// Find invalid IDs for part 2
+fn invalid_ids_any_length(min: u64, max: u64) -> Vec<u64> {
+    let max_str = max.to_string();
+
+    // NOTE: initially I was using a Vec and then used `are_halves_repeated` to detect duplicates,
+    // but that rejected too many numbers.
+    // let mut invalid_ids: Vec<u64> = Vec::new();
+    // There is some bug in `are_halves_repeated` and I can't find it. Thus, I'm using a HashSet.
+    // It's fast enough (both parts execute in 0.03s in release mode on my Mac).
+    let mut invalid_ids: HashSet<u64> = HashSet::new();
+
+    for sequence_length in 1..=(max_str.len() / 2) {
+        let mut repeated_digits: u64 = iter::once('1')
+            .chain(iter::repeat('0').take(sequence_length - 1))
+            .collect::<String>()
+            .parse()
+            .expect("Cannot parse starting repeated digits");
+
+        'increment_digits: loop {
+            #[cfg(test)]
+            dbg!(repeated_digits);
+
+            // if are_halves_repeated(&repeated_digits.to_string()) {
+            //     repeated_digits += 1;
+            //     continue;
+            // }
+            let repeated_digits_str = repeated_digits.to_string();
+            if repeated_digits_str.len() != sequence_length {
+                break 'increment_digits;
+            }
+
+            // let starting_sequence_repeats = min_str.len() / sequence_length;
+            let starting_sequence_repeats = 2;
+
+            'sequence_lengths: for sequence_repeats in
+                starting_sequence_repeats..=(max_str.len() / sequence_length)
+            {
+                let id = repeated_digits_str.repeat(sequence_repeats);
+                let id: u64 = id.parse().expect("Could not parse ID");
+
+                #[cfg(test)]
+                dbg!(id);
+
+                if id < min {
+                    continue;
+                }
+
+                if id > max {
+                    if sequence_repeats == starting_sequence_repeats {
+                        break 'increment_digits;
+                    } else {
+                        break 'sequence_lengths;
+                    }
+                }
+
+                #[cfg(test)]
+                println!("matches");
+                // invalid_ids.push(id);
+                invalid_ids.insert(id);
+            }
+
+            repeated_digits += 1;
+        }
+    }
+
+    let mut invalid_ids: Vec<_> = invalid_ids.into_iter().collect();
+
+    #[cfg(test)]
+    invalid_ids.sort();
+
+    invalid_ids
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{invalid_ids, parse_pair, part1};
+    use crate::{
+        are_halves_repeated, invalid_ids_any_length, invalid_ids_halves, parse_input, parse_pair,
+        part1, part2,
+    };
 
     #[test]
     fn test_parse_pair() {
@@ -97,19 +210,62 @@ mod tests {
     }
 
     #[test]
-    fn test_count_invalid_ids() {
-        assert_eq!(invalid_ids(11, 22), vec![11, 22]);
-        assert_eq!(invalid_ids(95, 115), vec![99]);
-        assert_eq!(invalid_ids(998, 1012), vec![1010]);
-        assert_eq!(invalid_ids(1188511880, 1188511890), vec![1188511885]);
-        assert_eq!(invalid_ids(1698522, 1698528), vec![]);
-        assert_eq!(invalid_ids(446443, 446449), vec![446446]);
-        assert_eq!(invalid_ids(38593856, 38593862), vec![38593859]);
+    fn test_count_invalid_ids_halves() {
+        assert_eq!(invalid_ids_halves(11, 22), vec![11, 22]);
+        assert_eq!(invalid_ids_halves(95, 115), vec![99]);
+        assert_eq!(invalid_ids_halves(998, 1012), vec![1010]);
+        assert_eq!(invalid_ids_halves(1188511880, 1188511890), vec![1188511885]);
+        assert_eq!(invalid_ids_halves(1698522, 1698528), vec![]);
+        assert_eq!(invalid_ids_halves(446443, 446449), vec![446446]);
+        assert_eq!(invalid_ids_halves(38593856, 38593862), vec![38593859]);
     }
 
     #[test]
+    fn test_are_halves_repeated() {
+        assert_eq!(are_halves_repeated("1212"), true);
+        assert_eq!(are_halves_repeated("555555"), true);
+        assert_eq!(are_halves_repeated("55555"), true);
+        assert_eq!(are_halves_repeated("123123"), true);
+        assert_eq!(are_halves_repeated("123124"), false);
+        assert_eq!(are_halves_repeated("1221"), false);
+        assert_eq!(are_halves_repeated("121"), false);
+    }
+
+    #[test]
+    fn test_count_invalid_ids_any_length() {
+        assert_eq!(invalid_ids_any_length(11, 22), vec![11, 22]);
+        assert_eq!(invalid_ids_any_length(95, 115), vec![99, 111]);
+        assert_eq!(invalid_ids_any_length(998, 1012), vec![999, 1010]);
+        assert_eq!(
+            invalid_ids_any_length(1188511880, 1188511890),
+            vec![1188511885]
+        );
+        assert_eq!(invalid_ids_any_length(222220, 222224), vec![222222]);
+        assert_eq!(invalid_ids_any_length(1698522, 1698528), vec![]);
+        assert_eq!(invalid_ids_any_length(446443, 446449), vec![446446]);
+        assert_eq!(invalid_ids_any_length(38593856, 38593862), vec![38593859]);
+        assert_eq!(invalid_ids_any_length(565653, 565659), vec![565656]);
+        assert_eq!(
+            invalid_ids_any_length(824824821, 824824827),
+            vec![824824824]
+        );
+        assert_eq!(
+            invalid_ids_any_length(2121212118, 2121212124),
+            vec![2121212121]
+        );
+    }
+
+    const EXAMPLE_INPUT: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+    #[test]
     fn test_part1_example() {
-        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
-        assert_eq!(part1(input), 1227775554);
+        let pairs = parse_input(EXAMPLE_INPUT);
+        assert_eq!(part1(&pairs), 1227775554);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let pairs = parse_input(EXAMPLE_INPUT);
+        assert_eq!(part2(&pairs), 4174379265);
     }
 }
