46ab2f57693243036ade3ff2ae272bd9ef3131fb
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-12-05 21:55:27 +0100

    chore(2025-05): add range joining operation

--- 2025/05/src/main.rs
+++ 2025/05/src/main.rs
@@ -1,7 +1,16 @@
-use std::ops::RangeInclusive;
+use std::{
+    io::{Read, stdin},
+    ops::RangeInclusive,
+};
 
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    stdin()
+        .read_to_string(&mut input)
+        .expect("stdin should be valid input");
+    let parsed_input = parse_input(&input);
+
+    dbg!(parsed_input);
 }
 
 #[derive(Debug, Default, PartialEq, Eq)]
@@ -40,9 +49,54 @@ fn parse_input(input: impl AsRef<str>) -> ParsedInput {
     parsed_input
 }
 
+#[derive(Debug, Default)]
+struct DisjointRanges {
+    // Disjoint ranges in ascending order
+    ranges: Vec<RangeInclusive<u64>>,
+}
+
+fn maybe_join_ranges(
+    r1: &RangeInclusive<u64>,
+    r2: &RangeInclusive<u64>,
+) -> Option<RangeInclusive<u64>> {
+    // https://doc.rust-lang.org/src/core/slice/mod.rs.html#5312
+    // with a twist - join when the ranges are neighboring (end of first + 1 = start of second)
+    let overlapping = *r1.start() <= (*r2.end() + 1) && *r2.start() <= (*r1.end() + 1);
+
+    if overlapping {
+        Some(RangeInclusive::new(
+            std::cmp::min(*r1.start(), *r2.start()),
+            std::cmp::max(*r1.end(), *r2.end()),
+        ))
+    } else {
+        None
+    }
+}
+
+impl DisjointRanges {
+    fn insert(&mut self, range: RangeInclusive<u64>) {
+        if self.ranges.is_empty() {
+            self.ranges.push(range);
+            return;
+        }
+
+        let insert_index = 0;
+
+        // TODO: find insert point
+
+        // TODO: insert
+
+        // TODO: join neighboring ranges
+    }
+
+    fn contains(&self, num: u64) -> bool {
+        todo!();
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{ParsedInput, parse_input};
+    use crate::{DisjointRanges, ParsedInput, maybe_join_ranges, parse_input};
 
     #[test]
     fn test_parse_input() {
@@ -65,5 +119,57 @@ mod tests {
                 numbers: vec![1, 5, 8, 11, 17, 32],
             }
         );
+    }
+
+    #[test]
+    fn test_maybe_join_ranges() {
+        assert_eq!(maybe_join_ranges(&(5..=6), &(6..=8)), Some(5..=8));
+        assert_eq!(maybe_join_ranges(&(6..=8), &(5..=6)), Some(5..=8));
+        assert_eq!(maybe_join_ranges(&(4..=4), &(6..=8)), None);
+        assert_eq!(maybe_join_ranges(&(6..=8), &(4..=4)), None);
+        assert_eq!(maybe_join_ranges(&(6..=8), &(7..=7)), Some(6..=8));
+        assert_eq!(maybe_join_ranges(&(7..=7), &(6..=8)), Some(6..=8));
+
+        // Join even if there is a difference of 1
+        assert_eq!(maybe_join_ranges(&(6..=8), &(9..=10)), Some(6..=10));
+        assert_eq!(maybe_join_ranges(&(9..=10), &(6..=8)), Some(6..=10));
+    }
+
+    #[test]
+    fn test_disjoint_ranges_join() {
+        let mut ranges = DisjointRanges::default();
+
+        ranges.insert(3..=5);
+        ranges.insert(5..=6);
+        ranges.insert(2..=5);
+
+        assert_eq!(ranges.ranges, vec![2..=6]);
+    }
+
+    #[test]
+    fn test_disjoint_ranges_join_two_neighbors() {
+        let mut ranges = DisjointRanges::default();
+
+        ranges.insert(2..=3);
+        ranges.insert(5..=6);
+
+        // Fill in the middle range
+        ranges.insert(4..=5);
+
+        assert_eq!(ranges.ranges, vec![2..=6]);
+    }
+
+    #[test]
+    fn test_disjoint_ranges_join_multiple() {
+        let mut ranges = DisjointRanges::default();
+
+        ranges.insert(2..=3);
+        ranges.insert(5..=6);
+        ranges.insert(8..=9);
+
+        // Overlapping range
+        ranges.insert(1..=10);
+
+        assert_eq!(ranges.ranges, vec![1..=10]);
     }
 }
