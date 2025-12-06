f55878969803ebe50a6a0ef5521cd47b04d6f7a5
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-12-06 13:52:55 +0100

    feat(2025-05): solve part 1

--- 2025/05/src/main.rs
+++ 2025/05/src/main.rs
@@ -10,7 +10,20 @@ fn main() {
         .expect("stdin should be valid input");
     let parsed_input = parse_input(&input);
 
-    dbg!(parsed_input);
+    println!("Part 1: {}", solve_part1(&parsed_input));
+}
+
+fn solve_part1(input: &ParsedInput) -> u32 {
+    let mut result = 0;
+
+    let ranges = DisjointRanges::from_iter(input.ranges.iter().cloned());
+    for num in input.numbers.iter() {
+        if ranges.contains(*num) {
+            result += 1;
+        }
+    }
+
+    result
 }
 
 #[derive(Debug, Default, PartialEq, Eq)]
@@ -75,28 +88,89 @@ fn maybe_join_ranges(
 
 impl DisjointRanges {
     fn insert(&mut self, range: RangeInclusive<u64>) {
-        if self.ranges.is_empty() {
-            self.ranges.push(range);
-            return;
+        self.insert_only(range);
+
+        #[cfg(debug_assertions)]
+        self.ensure_valid();
+    }
+
+    fn insert_only(&mut self, mut range: RangeInclusive<u64>) {
+        let mut index = 0;
+        while index < self.ranges.len() {
+            let other_range = &self.ranges[index];
+
+            if *range.start() > *other_range.end() + 1 {
+                index += 1;
+                continue;
+            }
+
+            if let Some(joined_range) = maybe_join_ranges(&range, other_range) {
+                range = joined_range;
+                self.ranges.remove(index);
+                // NOTE: do not increment the index here.
+                // Removing the range at `index` will shift all elements left,
+                // so `index` will now point to the next range.
+            } else {
+                // NOTE: range < other_range, so insert `range` before `other_range`
+                self.ranges.insert(index, range);
+                return;
+            }
         }
 
-        let insert_index = 0;
-
-        // TODO: find insert point
-
-        // TODO: insert
-
-        // TODO: join neighboring ranges
+        self.ranges.push(range);
     }
 
     fn contains(&self, num: u64) -> bool {
-        todo!();
+        for range in self.ranges.iter() {
+            if num < *range.start() {
+                return false;
+            }
+
+            if num <= *range.end() {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn ensure_valid(&self) {
+        for range_pair in self.ranges.windows(2) {
+            let [r1, r2] = range_pair else {
+                panic!("window did not contain two ranges")
+            };
+
+            assert!(r1.start() < r2.start(), "{r1:?} must start before {r2:?}");
+
+            assert!(
+                r1.end() < r2.start(),
+                "{r1:?} must end before {r2:?} starts"
+            );
+
+            let distance = r2.start() - r1.end();
+            assert!(
+                distance > 1,
+                "distance between {r1:?} and {r2:?} must be greater than 1, found {distance}"
+            );
+        }
+    }
+}
+
+impl FromIterator<RangeInclusive<u64>> for DisjointRanges {
+    fn from_iter<T: IntoIterator<Item = RangeInclusive<u64>>>(iter: T) -> Self {
+        let mut r = Self::default();
+
+        for v in iter {
+            r.insert(v);
+        }
+
+        r
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{DisjointRanges, ParsedInput, maybe_join_ranges, parse_input};
+    use crate::{DisjointRanges, ParsedInput, maybe_join_ranges, parse_input, solve_part1};
 
     #[test]
     fn test_parse_input() {
@@ -119,6 +193,16 @@ mod tests {
                 numbers: vec![1, 5, 8, 11, 17, 32],
             }
         );
+    }
+
+    #[test]
+    fn test_solve_part1() {
+        let input = ParsedInput {
+            ranges: vec![3..=5, 10..=14, 16..=20, 12..=18],
+            numbers: vec![1, 5, 8, 11, 17, 32],
+        };
+
+        assert_eq!(solve_part1(&input), 3);
     }
 
     #[test]
@@ -171,5 +255,23 @@ mod tests {
         ranges.insert(1..=10);
 
         assert_eq!(ranges.ranges, vec![1..=10]);
+    }
+
+    #[test]
+    fn test_disjoint_ranges_contains() {
+        let ranges = DisjointRanges::from_iter(vec![1..=5, 7..=10]);
+
+        assert_eq!(ranges.contains(0), false);
+        assert_eq!(ranges.contains(1), true);
+        assert_eq!(ranges.contains(2), true);
+        assert_eq!(ranges.contains(3), true);
+        assert_eq!(ranges.contains(4), true);
+        assert_eq!(ranges.contains(5), true);
+        assert_eq!(ranges.contains(6), false);
+        assert_eq!(ranges.contains(7), true);
+        assert_eq!(ranges.contains(8), true);
+        assert_eq!(ranges.contains(9), true);
+        assert_eq!(ranges.contains(10), true);
+        assert_eq!(ranges.contains(11), false);
     }
 }
