2208fbbbef208a671316baf5d2a930fa75db6d48
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-12-11 19:37:15 +0100

    feat(2025-11): solve part 1

--- 2025/11/src/main.rs
+++ 2025/11/src/main.rs
@@ -12,7 +12,51 @@ fn main() {
 }
 
 fn solve_part1(graph: &Graph) -> u32 {
-    todo!()
+    struct Solver<'a> {
+        paths_to_out_count: Vec<u32>,
+        visited: Vec<bool>,
+        graph: &'a Graph,
+    }
+
+    impl<'a> Solver<'a> {
+        fn visit(&mut self, index: usize) {
+            self.visited[index] = true;
+            let node = &self.graph.nodes[index];
+
+            for &neighbor_index in node.neighbor_indexes.iter() {
+                if !self.visited[neighbor_index] {
+                    self.visit(neighbor_index);
+                }
+
+                self.paths_to_out_count[index] += self.paths_to_out_count[neighbor_index];
+            }
+        }
+    }
+
+    let out_node_index = *graph
+        .node_indexes_by_name
+        .get("out")
+        .expect("'out' node should exist");
+
+    let mut paths_to_out_count = vec![0; graph.nodes.len()];
+    paths_to_out_count[out_node_index] = 1;
+
+    let mut visited = vec![false; graph.nodes.len()];
+    visited[out_node_index] = true;
+
+    let mut solver = Solver {
+        graph,
+        paths_to_out_count,
+        visited,
+    };
+
+    let start_node_index = *graph
+        .node_indexes_by_name
+        .get("you")
+        .expect("'you' node should exist");
+    solver.visit(start_node_index);
+
+    solver.paths_to_out_count[start_node_index]
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -32,7 +76,7 @@ impl Graph {
     fn insert_edges(&mut self, from: &str, neighbors: &[&str]) {
         let neighbor_indexes: Vec<usize> = neighbors
             .into_iter()
-            .map(|name| self.get_node_index(name))
+            .map(|name| self.get_or_create_node_index(name))
             .collect();
         let from_node = self.get_node_mut(from);
 
@@ -42,11 +86,11 @@ impl Graph {
     }
 
     fn get_node_mut<'a>(&'a mut self, name: &str) -> &'a mut GraphNode {
-        let index = self.get_node_index(name);
+        let index = self.get_or_create_node_index(name);
         self.nodes.get_mut(index).expect("node should exist")
     }
 
-    fn get_node_index(&mut self, name: &str) -> usize {
+    fn get_or_create_node_index(&mut self, name: &str) -> usize {
         match self.node_indexes_by_name.get(name) {
             Some(&index) => index,
             None => {
@@ -134,14 +178,37 @@ ggg: out
 hhh: ccc fff iii
 iii: out";
 
-        let mut graph = Graph::parse(input.lines()).expect("input should be valid");
+        let graph = Graph::parse(input.lines()).expect("input should be valid");
 
         assert_eq!(
             graph.nodes.len(),
             // NOTE: 10 nodes that have neighbors, and "out" that is the final node
             11
         );
-        assert_eq!(graph.get_node_mut("out").neighbor_indexes, vec![]);
-        assert_eq!(graph.get_node_mut("aaa").neighbor_indexes.len(), 2);
+
+        fn get_node<'a>(graph: &'a Graph, name: &str) -> Option<&'a GraphNode> {
+            let index = *graph.node_indexes_by_name.get(name)?;
+            graph.nodes.get(index)
+        }
+        assert_eq!(get_node(&graph, "out").unwrap().neighbor_indexes, vec![]);
+        assert_eq!(get_node(&graph, "aaa").unwrap().neighbor_indexes.len(), 2);
+    }
+
+    #[test]
+    fn test_solve_part1() {
+        let input = "aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out";
+
+        let graph = Graph::parse(input.lines()).expect("input should be valid");
+
+        assert_eq!(solve_part1(&graph), 5);
     }
 }
